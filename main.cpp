f9df64dea63e1f30932c210ab9d2bd23389d4d01
TRKKL <mehmeteminturkkal@gmail.com>
2025-03-01 14:21:59 +0200

    İlk commit: Proje GitHub'a yüklendi

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include "MerkelMain.h"
+#include "CSVReader.h"
+#include "Wallet.h"
+
+
+int main(){
+
+    
+
+    MerkelMain app{};
+    app.init();
+    
+
+}
